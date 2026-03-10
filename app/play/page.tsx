9ce8471cee7837b9f3ca3f5ca529640d75b65311
Selin Irmak <selin.irmak89@gmail.com>
2026-03-11 00:37:52 +0300

    fix play page component

---
+++ app/play/page.tsx
@@ -0,0 +1,18 @@
+export default function PlayPage() {
+  return (
+    <main
+      style={{
+        minHeight: "100vh",
+        background: "#070b16",
+        color: "white",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: "40px",
+        letterSpacing: "4px",
+      }}
+    >
+      CAELINUS PLAY
+    </main>
+  );
+}
