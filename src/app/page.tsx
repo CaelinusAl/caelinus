4d4abb872577f5d33df55116d0fca367e1405468
Selin Irmak <selin.irmak89@gmail.com>
2026-03-11 00:24:34 +0300

    initial caelinus nextjs project

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+export default function Page() {
+  return (
+    <div style={{
+      minHeight: "100vh",
+      background: "#070b16",
+      color: "white",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      fontSize: "42px",
+      letterSpacing: "4px"
+    }}>
+      CAELINUS PLAY
+    </div>
+  )
+}
---
+++ src/app/play/page.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function PlayPage() {
+  return (
+    <div
+      style={{
+        minHeight: "100vh",
+        background: "#070b16",
+        color: "white",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: "40px",
+        letterSpacing: "4px"
+      }}
+    >
+      CAELINUS PLAY
+    </div>
+  );
+}
